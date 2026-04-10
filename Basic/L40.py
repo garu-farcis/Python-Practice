acf983bd178e8f0420eced6647efc02732085340
garu-farcis <priyankagaru2024@gmail.com>
2026-04-10 16:35:48 +0200

    Lists

--- Basic/L40.py
+++ Basic/L40.py
@@ -5,6 +5,15 @@ on the left. Another way is shown below on the right.
 (2,0) (2,1) (2,2) 6 7 8
 (a) Write some code that translates from the left representation to the right one. The //and
 %operators will be useful. Be sure your code works for arrays of any size.
+row = index // number_of_columns
+col = index % number_of_columns
+index = row * number_of_columns + column
 (b) Write some code that translates from the right representation to the left one."""
 
-
+L = [[(0,0), (0,1), (0,2)], [(1,0), (1,1), (1,2)], [(2,0), (2,1), (2,2)]]
+print(L)
+Lnew= [j for m in L for j in m]
+print(Lnew)
+cols = 3
+indices = [row * cols + col for (row, col) in Lnew]
+print(indices)
