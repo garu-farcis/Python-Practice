9dc5fbfbd56ec8715139ba5832461a21560d98c3
garu-farcis <priyankagaru2024@gmail.com>
2026-04-13 15:28:02 +0200

    Lists

--- Basic/L37.py
+++ Basic/L37.py
@@ -3,3 +3,15 @@ following:
 (a) Print the list.
 (b) Find the largest value in the third row.
 (c) Find the smallest value in the sixth column."""
+from random import randint
+from pprint import pprint
+L =[[(randint(1,100)) for i in range(10)] for j in range(10)]
+pprint(L)
+row =2
+val = [L[row][i] for i in range(len(L))]
+pprint(val)
+print("max of 3rd row is ", max(val))
+col = 5
+val1 = [L[i][col] for i in range(len(L))]
+pprint(val1)
+print("min of 6th col is ", min(val1))
--- Basic/L38.py
+++ Basic/L38.py
@@ -2,3 +2,24 @@
 five-digit number n that is such that every one of the following ten numbers shares exactly
 one digit in common in the same position as n. Find n.
 01265, 12171, 23257, 34548, 45970, 56236, 67324, 78084, 89872, 99414"""
+from pprint import pprint
+L= [ 10265, 12171, 23257, 34548, 45970, 56236, 67324, 78084, 89872, 99414]
+for num in range(100000):
+    candidate = str(num).zfill(5)
+
+    ok = True
+
+    for row in L:
+        matches = 0
+
+        for i in range(5):
+            if candidate[i] == row[i]:
+                matches += 1
+
+        if matches != 1:
+            ok = False
+            break
+
+    if ok:
+        print(candidate)
+        break
