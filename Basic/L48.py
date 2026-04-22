8aba8d753690950fb6abaf19f58c8ef458f5b944
garu-farcis <priyankagaru2024@gmail.com>
2026-04-22 17:19:09 +0200

    Lists & Dicts

--- Basic/L48.py
+++ Basic/L48.py
@@ -1,2 +1,16 @@
 """Write a program that finds all pairs of six-digit palindromic numbers that are less than 20
 apart. One such pair is 199991 and 200002"""
+
+
+my_list = []
+
+for i in range(100000, 1000000):
+    digits = []
+    for ch in str(i):
+        digits.append(int(ch))
+    my_list.append(digits)
+
+
+my_list = [list(map(int, str(i))) for i in range(100000, 1000000)]
+L=[i for i in my_list if i==i[::-1]]
+print(L)
