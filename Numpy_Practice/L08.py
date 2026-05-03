50543a868c711adc954fafc10f8485f13533e52d
garu-farcis <priyankagaru2024@gmail.com>
2026-05-03 18:31:09 +0200

    DSA

--- Numpy_Practice/L08.py
+++ Numpy_Practice/L08.py
@@ -55,6 +55,7 @@ print(L)
 """Insert Sorting Algorithm"""
 L=[89,12,9,74,276,847,74,273,773,93,1004,74747]
 my_len=len(L)
+# keys=70
 # keys=0
 for i in range(0,my_len):
     keys=L[i]
@@ -68,6 +69,41 @@ print(L)
 """Reverse Array"""
 L=[89,12,9,74,276,847,74,273,773,93,1004,74747]
 print(L[::-1])
+for i in range(0,len(L)//2):
+    L[i],L[my_len-i-1]=L[my_len-i-1],L[i]
+print(L)
 
 
 """Dichotomy Binary Search"""
+
+#  linear search
+L=[89,12,9,74,276,847,74,273,773,93,1004,74747]
+my_ind=74
+val= [i for i in range(len(L)) if L[i]==my_ind]
+print(val)
+
+# binary search can be performed only on sorted arrays
+L=[89,12,9,74,276,847,74,273,773,93,1004,74747]
+L.sort()
+print(f'sorted {L}'.format(L))
+low=0
+high=len(L)-1
+# mid=(low+high)//2
+# print(mid)
+search_val=74
+found=False
+while low<=high:
+    mid = (low + high) // 2
+    if L[mid]==search_val:
+        print("Found at index:", mid)
+        found = True
+        break
+    elif L[mid]<search_val:
+        low=mid+1
+    else:
+        high =mid-1
+if not found:
+    print('not found')
+
+"""Shell Sorting"""
+
