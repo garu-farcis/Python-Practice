6b70d4f2e84927014f59f1e8e02b8b9400300c99
garu-farcis <priyankagaru2024@gmail.com>
2026-05-11 18:29:02 +0200

    Numpy Concepts

---
+++ Numpy_Practice/L13.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+points = np.arange(-5, 5, 0.01)
+xs, ys = np.meshgrid(points, points)
+z = np.sqrt(xs ** 2 + ys ** 2)
+plt.imshow(z, cmap=plt.cm.gray, extent=[-5, 5, -5, 5])
+plt.colorbar()
+plt.title(f"Image plot of $\sqrt{x^2 + y^2}$ for a grid of values")
