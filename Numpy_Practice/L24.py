8cac0615c58659927aa1fbafa013c5b24e217e42
garu-farcis <priyankagaru2024@gmail.com>
2026-05-31 20:30:07 +0200

    Numpy Concepts

--- Numpy_Practice/L24.py
+++ Numpy_Practice/L24.py
@@ -1,1 +1,14 @@
 """numpy questions1: """
+import numpy as np
+
+rng = np.random.default_rng(42)
+normal_samples = rng.standard_normal(1000)
+
+print(normal_samples.shape)
+print(normal_samples[:5])
+uniform_matrix = rng.random((10, 10))
+
+print(uniform_matrix)
+data = rng.standard_normal((100, 5))
+corr_matrix = np.corrcoef(data, rowvar=False)
+print("Correlation Matrix:\n", corr_matrix)
