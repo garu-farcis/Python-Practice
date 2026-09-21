0364b724b2a9918a81707f0f0b9b80911b5fddf3
garu-farcis <priyankagaru2024@gmail.com>
2026-09-21 17:44:13 +0200

    update

--- Pandas/L10.py
+++ Pandas/L10.py
@@ -1,2 +1,14 @@
 """Calculate a 3-order rolling average of revenue for each customer (sorted by order_date).
 Include only customers who have at least 4 orders and show the original revenue alongside"""
+
+file_path="/Users/prse/PycharmProjects/Python-Refresher/Python-Practice/data/sales_data.csv"
+
+import pandas as pd
+pd.set_option('display.max_columns', None)
+
+df=pd.read_csv(file_path)
+df['revenue']=df['quantity']*df['price']
+df['cust_four']=df[df['quanity']>=4]
+cust_4=df.groupby('customer_id')['cust_four'].nunique()
+# cust_filt=df.groupby('customer_id')
+print(cust_4)
